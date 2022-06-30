6b3b615760e55562175d43a99b1fdc6692f3a257
leonrenn <leon.ransf@gmail.com>
2022-06-30 12:34:40 +0200

    new feature for password modification

--- passwordGenerator.py
+++ passwordGenerator.py
@@ -9,13 +9,15 @@ import random
 # MAIN
 
 def main() -> None:
+    opt = "all"
     while True:
         try:
             number_of_characters = int(input("How many characters?"))
             break
         except TypeError:
             print("Provide a valid argument!\n")
-    characters = [chr(i) for i in range(33,127)] # beginning with !
+    if opt == "all":
+        characters = [chr(i) for i in range(33,127)] # beginning with !
     password = [random.choice(characters) for _ in range(number_of_characters)]
     print("".join(password))
     return
